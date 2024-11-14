7619d9ed49da59ca53f864fb4d577f615b4c735a
Adam Rębowicz <arebowicz@gmail.com>
2024-11-14 06:50:54 +0100

    ver 4.0

--- Zadanie 01 - ver 3.0/scripts/scripts.js
+++ Zadanie 01 - ver 3.0/scripts/scripts.js
@@ -102,3 +102,125 @@ document.querySelector('#ingredients .section-content').addEventListener('mouseo
         }
     }
 });
+
+document.querySelector('#dropdown-button').addEventListener('click', (event) => {
+    if (document.querySelector('#dropdown-items').classList[0] == 'hide-dropdown') {
+        document.querySelector('#dropdown-button img').src = 'images/icons/X-dropdown-icon.png';
+    } else {
+        document.querySelector('#dropdown-button img').src = 'images/icons/dropdown-icon.png'
+    }
+    document.querySelector('#dropdown-items').classList.toggle('show-dropdown');
+    document.querySelector('#dropdown-items').classList.toggle('hide-dropdown');
+});
+
+let pageSize = 8;
+let pageNumber = 1;
+
+document.querySelector('#dropdown-items').addEventListener('click', (event) => {
+    // note: it's possible to click on container
+    if (event.target.id == 'dropdown-item1'
+        || event.target.id == 'dropdown-item2'
+        || event.target.id == 'dropdown-item3') {
+            pageSize = Number(event.target.dataset.value);
+        document.querySelector('#dropdown-button img').src = 'images/icons/dropdown-icon.png'
+        document.querySelector('#dropdown-items').classList.toggle('show-dropdown');
+        document.querySelector('#dropdown-items').classList.toggle('hide-dropdown');
+        document.querySelector('#dropdown-button').replaceChild(document.createTextNode(`${pageSize} `), document.querySelector('#dropdown-button').childNodes[0]);
+        document.querySelector('#products .section-content').replaceChildren();
+        pageNumber = 1;
+        fetchData();
+    }
+});
+
+let isFetching = false;
+
+function fetchData() {
+    if (isFetching) {
+        return;
+    }
+    isFetching = true;
+    const timeoutId = setTimeout(() => {
+        console.error('The fetch request timed out');
+        // display a timeout message to the user
+        let timeoutErr = document.createElement('h3');
+        timeoutErr.appendChild(document.createTextNode('Problem z wczytaniem danych. Spróbuj ponownie później lub skontaktuj się z administratorem.'));
+        document.querySelector('#products .section-content').appendChild(timeoutErr);
+        isFetching = false;
+    }, 5000);
+    fetch(`https://brandstestowy.smallhost.pl/api/random?pageNumber=${pageNumber}&pageSize=${pageSize}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            clearTimeout(timeoutId);
+            // process the data and update webpage
+            if (pageNumber <= data.totalPages) {
+                const productsSectionContent = document.querySelector('#products .section-content');
+                if (pageNumber > 1) {
+                    productsSectionContent.appendChild(document.createElement('hr'));
+                    let pageNumberH3 = document.createElement('h3');
+                    pageNumberH3.appendChild(document.createTextNode(`Strona ${pageNumber}:`));
+                    productsSectionContent.appendChild(pageNumberH3);
+                }
+                let productsWrapper = document.createElement('div');
+                productsWrapper.classList.add('products-wrapper');
+                data.data.forEach((item) => {
+                    let current = document.createElement('div');
+                    current.classList.add('product');
+                    current.dataset.id = item.id;
+                    current.dataset.text = item.text;
+                    current.appendChild(document.createTextNode(`ID: ${item.id}`));
+                    productsWrapper.appendChild(current);
+                });
+                productsSectionContent.appendChild(productsWrapper);
+                if (pageNumber == data.totalPages) {
+                    let endOfPages = document.createElement('h3');
+                    endOfPages.appendChild(document.createTextNode('Wczytano wszystkie strony'));
+                    productsSectionContent.appendChild(endOfPages);
+                }
+                pageNumber++;
+            }
+            isFetching = false;
+        })
+        .catch(error => {
+            // display error message to the user
+            let err = document.createElement('h3');
+            err.appendChild(document.createTextNode('Problem z API. Spróbuj ponownie później lub skontaktuj się z administratorem.'));
+            document.querySelector('#products .section-content').appendChild(err);
+            isFetching = false;
+        });
+}
+
+window.addEventListener('scroll', (event) => {
+    const productsSection = document.querySelector('#products');
+    if (productsSection.getBoundingClientRect().top <= window.innerHeight) {
+        if (pageNumber == 1) {
+            fetchData();
+        } else {
+            const lastElWrapper = document.querySelectorAll('#products .products-wrapper');
+            const elWrapperChildren = lastElWrapper[lastElWrapper.length - 1].children;
+            const lastEl = elWrapperChildren[elWrapperChildren.length - 1];
+            if (lastEl.getBoundingClientRect().top <= window.innerHeight) {
+                fetchData();
+            }
+        }
+    }
+});
+
+document.querySelector('#products .section-content').addEventListener('click', (event) => {
+    if (event.target.classList.contains('product')) {
+        document.querySelector('#product-popup').classList.toggle('popup-active');
+        document.querySelector('body').classList.toggle('noscroll');
+        let newId = document.createTextNode(event.target.dataset.id);
+        document.querySelector('#inject-popup-id').replaceChildren();
+        document.querySelector('#inject-popup-id').appendChild(newId);
+    }
+});
+
+document.querySelector('#product-popup .popup-close').addEventListener('click', (event) => {
+    document.querySelector('#product-popup').classList.toggle('popup-active');
+    document.querySelector('body').classList.toggle('noscroll');
+});
